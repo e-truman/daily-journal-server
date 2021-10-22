b5df4863a2d2b499584b22124ace19076da2bf32
Erin <etruman2@gmail.com>
2021-10-22 13:52:52 -0500

    added a search entry and create entry class. writing an update entry class

--- entries/request.py
+++ entries/request.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Entry
+from models import Entry, Mood
 
 
 def get_all_entries():
@@ -16,11 +16,14 @@ def get_all_entries():
         # Write the SQL query to get the information you want
         db_cursor.execute("""
         SELECT
-            a.id,
-            a.date,
-            a.entry,
-            a.mood_id
-        FROM journal a
+            j.id,
+            j.date,
+            j.entry,
+            j.mood_id,
+            m.label
+        FROM journal j
+        JOIN Mood m
+            ON j.mood_id = m.id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -38,7 +41,8 @@ def get_all_entries():
             # Animal class above.
             entry = Entry(row['id'], row['date'], row['entry'],
                             row['mood_id'])
-
+            mood = Mood(row['mood_id'], row['label'])
+            entry.mood=mood.__dict__
             entries.append(entry.__dict__)
 
     # Use `json` package to properly serialize list as JSON
@@ -55,12 +59,15 @@ def get_single_entry(id):
         # into the SQL statement.
         db_cursor.execute("""
         SELECT
-            a.id,
-            a.date,
-            a.entry,
-            a.mood_id
-        FROM journal a
-        WHERE a.id = ?
+            j.id,
+            j.date,
+            j.entry,
+            j.mood_id,
+            m.label
+        FROM journal j
+        JOIN Mood m
+            ON m.id = j.mood_id 
+        WHERE j.id = ?
         """, ( id, ))
 
         # Load the single result into memory
@@ -69,7 +76,8 @@ def get_single_entry(id):
         # Create an entry instance from the current row
         entry = Entry(data['id'], data['date'], data['entry'],
                             data['mood_id'])
-
+        mood = Mood(data['mood_id'], data['label'])
+        entry.mood=mood.__dict__
         return json.dumps(entry.__dict__)
 
 def delete_entry(id):
@@ -81,4 +89,74 @@ def delete_entry(id):
         WHERE id = ?
         """, (id, ))
 
+def search_entries(searchTerm):
+    # Open a connection to the database
+    with sqlite3.connect("./journal.db") as conn: # allows to use sqlite3 with package we imported. use .connect to interact with database. use with as syntax
 
+        # Just use these. It's a Black Box.
+        # on conn class instance, row factory is a property or method. set equal to sqlite3.row. tells to get rows out of database. isntructions for how to get row
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        #conn.cursor after we know how to get rows. have to do this conn.cursor and execute in order. makes environment to execute query
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            j.id,
+            j.date,
+            j.entry,
+            j.mood_id,
+            m.label
+        FROM journal j
+        JOIN Mood m
+            ON j.mood_id = m.id
+        WHERE j.entry LIKE ?
+        """, (f"%{searchTerm}%",))
+
+        # Initialize an empty list to hold all animal representations
+        entries = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall() # gets everything in select statement
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            entry = Entry(row['id'], row['date'], row['entry'],
+                            row['mood_id'])
+            mood = Mood(row['mood_id'], row['label'])
+            entry.mood=mood.__dict__
+            entries.append(entry.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(entries)
+
+
+def create_entry(new_entry):
+    with sqlite3.connect("./journal.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO Journal
+            ( date, entry, mood_id)
+        VALUES
+            ( ?, ?, ?);
+        """, (new_entry['date'], new_entry['entry'],
+              new_entry['mood_id'], ))
+
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
+
+        # Add the `id` property to the entry dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_entry['id'] = id
+
+
+    return json.dumps(new_entry)
--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,5 @@
-from entries import get_all_entries, get_single_entry, delete_entry
-
+from entries import get_all_entries, get_single_entry, delete_entry, search_entries, create_entry
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer # classes coming from python, use properties on those classes
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -11,26 +11,36 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It gives a description of the class or function
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
+    
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
         path_params = path.split("/")
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
-        try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
 
-        return (resource, id)  # This is a tuple
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return ( resource, key, value )
+
+        # No query string parameter
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  # No route parameter exists: /animals
+            except ValueError:
+                pass  # Request had trailing slash: /animals/
+
+            return (resource, id)
+
 
     
 
@@ -66,51 +76,64 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any GET request.
     def do_GET(self):
         self._set_headers(200)
-        response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
+        response = {}
 
-        if resource == "entries":
-            if id is not None:
-                response = f"{get_single_entry(id)}"
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
 
-            else:
-                response = f"{get_all_entries()}"
+        # Response from parse_url() is a tuple with 2
+        # items in it, which means the request was for
+        # `/animals` or `/animals/2`
+        if len(parsed) == 2:
+            ( resource, id ) = parsed
 
+            if resource == "entries":
+                if id is not None:
+                    response = f"{get_single_entry(id)}"
+                else:
+                    response = f"{get_all_entries()}"
+
+        # Response from parse_url() is a tuple with 3
+        # items in it, which means the request was for
+        # `/resource?parameter=value`
+        elif len(parsed) == 3:
+            ( resource, key, value ) = parsed
+
+            # Is the resource `customers` and was there a
+            # query parameter that specified the customer
+            # email as a filtering value?
+            if key == "searchTerm":
+                response = search_entries(value)
 
         self.wfile.write(response.encode())
 
+
+
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
-    # def do_POST(self):
-    #     self._set_headers(201)
-    #     content_len = int(self.headers.get('content-length', 0))
-    #     post_body = self.rfile.read(content_len)
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
 
-    #     # Convert JSON string to a Python dictionary
-    #     post_body = json.loads(post_body)
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
 
-    #     # Parse the URL
-    #     (resource, id) = self.parse_url(self.path)
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
 
-    #     # Initialize new animal
-    #     new_animal = None
-    #     new_customer = None
-    #     new_employee = None
+        # Initialize new animal
+        new_entry = None
+       
 
-        # Add a new animal to the list. Don't worry about
-        # the orange squiggle, you'll define the create_animal
+        # Add a new entry to the list. Don't worry about
+        # the orange squiggle, you'll define the create_entry
         # function next.
-        # if resource == "animals":
-        #     new_animal = create_animal(post_body)
-        #     self.wfile.write(f"{new_animal}".encode())
-        # if resource == "employees":
-        #     new_employee = create_employee(post_body) 
-        #     self.wfile.write(f"{new_employee}".encode())           
-        # if resource == "customers":
-        #     new_customer = create_customer(post_body)
-        #     self.wfile.write(f"{new_customer}".encode())
+
+        new_entry = create_entry(post_body)
+        self.wfile.write(f"{new_entry}".encode())
+    
 
 
     # Here's a method on the class that overrides the parent's method.
